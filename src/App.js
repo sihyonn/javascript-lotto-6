80ea2f962dc051481160f392e4de121d8188cbc4
sihyonn <sihyeon982@naver.com>
2023-11-08 20:40:42 +0900

    feat: App.js에 구매금액 및 로또번호 발행 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import MakeLotto from './MakeLotto.js';
+import { Input } from './UI/Input.js';
+
 class App {
-  async play() {}
+  async play() {
+    let validAmount = false;
+    let validWinningNum = false;
+    let validBounsNum = false;
+
+    let amount;
+    let winningNum;
+    let lottos;
+    let bonusNum;
+
+    while (!validAmount) {
+      try {
+        amount = await Input.getPurchaseAmout();
+        const makeLotto = new MakeLotto([1, 2, 3, 4, 5, 6], amount);
+        lottos = await makeLotto.makeLottos();
+        validAmount = true;
+      } catch (e) {}
+    }
+  }
 }
 
 export default App;
