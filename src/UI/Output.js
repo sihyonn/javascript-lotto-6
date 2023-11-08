f14325d0f72d67da6de9903450cb0276515ee07a
sihyonn <sihyeon982@naver.com>
2023-11-08 14:26:53 +0900

    feat: Output.js( 구매수량, 발행된로또리스트, 당첨통계 및 수익률 출력 기능 추가)

---
+++ src/UI/Output.js
@@ -0,0 +1,29 @@
+import { Console } from '@woowacourse/mission-utils';
+import { OUTPUT } from '../constants/Messages.js';
+
+export const Output = {
+  // 발행한 로또 구매개수 출력
+  printPurchaseQuantity(quantity) {
+    Console.print(`${quantity}` + OUTPUT.PURCHASE);
+  },
+
+  // 발행한 로또번호리스트 출력
+  printLottoList(lottos) {
+    lottos.forEach((lotto) => {
+      Console.print(lotto);
+    });
+  },
+
+  // 당첨통계 출력
+  printWinningStatistics(winningInfo) {
+    Console.print(OUTPUT.WINNING_STATS);
+    OUTPUT.RESULT.forEach((v, idx) => {
+      Console.print(OUTPUT.RESULT + `${winningInfo[idx]}개`);
+    });
+  },
+
+  // 수익률 출력
+  printRateOfReturn(rate) {
+    Console.print(OUTPUT.RETURN_RATE(rate));
+  },
+};
