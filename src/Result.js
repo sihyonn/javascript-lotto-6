7db0b49f650b75b25bb38673ea300d3471f2a007
sihyonn <sihyeon982@naver.com>
2023-11-08 19:16:55 +0900

    feat: 일치여부 객체 업데이트 함수 작성

--- src/Result.js
+++ src/Result.js
@@ -41,5 +41,32 @@ class Result extends Lotto {
     ).length;
     return count;
   }
+  updateMatchObj(matchObj, count, curLotto) {
+    switch (count) {
+      case 3:
+        matchObj.three += 1;
+        break;
+      case 4:
+        matchObj.four += 1;
+        break;
+      case 5:
+        curLotto.includes(this.#bonusNum)
+          ? (matchObj.bonus += 1)
+          : (matchObj.five += 1);
+        break;
+      case 6:
+        matchObj.six += 1;
+        break;
+    }
+  }
+
+  async #isMatch() {
+    const matchObj = this.#initializeMatchObj();
+    this.#lottos = this.countMatchNumbers((curLotto) => {
+      const count = this.countMatchNumbers(curLotto);
+      this.updateMatchObj(matchObj, count, curLotto);
+    });
+    return matchObj;
+  }
 }
 export default Result;
